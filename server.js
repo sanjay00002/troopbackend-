abb03cc9c117984dbeee06fc61dc577de1791dd1
Pratik Nimje <pratiknimje@troopapp.in>
2023-06-12 15:06:03 +0530

    Server connected

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://troop:troop@cluster0.aljji9z.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+// const contestRouter = require('./Routes/Contest');
+// const userRouter = require('./Routes/User');
+
+
+// app.use('/contests' , contestRouter);
+// app.use('/users', userRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`);
+  });
