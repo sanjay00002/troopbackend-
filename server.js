6198131cfa59b6a06391fef0960da4c3a1207597
akash-banger2 <137279309+akash-banger2@users.noreply.github.com>
2023-06-23 17:00:19 +0530

    socket,  server side for chat

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import { Sequelize } from 'sequelize';
+const http = require('http');
+const socketIO = require('socket.io');
 
 import authRouter from './src/routes/auth';
 import userRouter from './src/routes/user';
@@ -9,9 +11,15 @@ import botRouter from './src/routes/bot';
 
 // require('dotenv').config({ path: './.env.local' });
 require('dotenv').config({ path: './.env' });
-
 const app = express();
-const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(json());
@@ -32,6 +40,32 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/contest', contestRouter);
 app.use('/api/v1/bot', botRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
+// app.listen(port, () => {
+//   console.log(`Server is running at port: ${port}`);
+// });
+
+
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for 'chat message' events from the client
+  socket.on('chat message', (message) => {
+    console.log('Received message:', message.content);
+
+    // Broadcast the received message to all connected clients, including the sender
+    // Later will change it to Broadcast to only the sender and the receiver
+    io.emit('chat message', message);
+  });
+
+
+  // Listen for 'disconnect' events from clients
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
