efbb5d38fbf909cd4f56c7f2071a01e9f15eac47
Pratik Nimje <pratiknimje@troopapp.in>
2023-07-05 12:45:55 +0530

    livecontestuserpool table created

---
+++ src/database/migrations/20230705051722-live-contest-instaces.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('LiveContestUserPool', { 
+      id:{
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      contest_id:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      socket_id:{
+        type:Sequelize.STRING,
+        allowNull:false,
+      },
+      stock_id:{
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      matched: {
+        type: Sequelize.BOOLEAN,
+        allowNull:true,
+      }
+     });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+    */
+    await queryInterface.dropTable('LiveContestUserPool');
+  }
+};
---
+++ src/database/migrations/20230705064506-live-contest-instaces-foreign-key-user.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint('LiveContestUserPool', {
+      fields: ['user_id'],
+      type: 'foreign key',
+      name: 'user_live_fk_constraint',
+      references: {
+        table: 'Users',
+        field: 'id',
+      },
+    });
+
+    await queryInterface.addConstraint('LiveContestUserPool',{
+      fields: ['contest_id'],
+      type: 'foreign key',
+      name: 'contest_live_fk_constraint',
+      references:{
+        table: 'Contests',
+        field: 'id',
+      }
+    });
+
+    await queryInterface.addConstraint('LiveContestUserPool',{
+      fields: ['stock_id'],
+      type: 'foreign key',
+      name: 'stock_live_fk_constraint',
+      references:{
+        table: 'Stocks',
+        field: 'id',
+      }
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeConstraint(
+      'LiveContestUserPool',
+      'user_live_fk_constraint',
+    );
+
+    await queryInterface.removeConstraint(
+      'LiveContestUserPool',
+      'contest_live_fk_constraint',
+    );
+
+    await queryInterface.removeConstraint(
+      'LiveContestUserPool',
+      'stock_live_fk_constraint',
+    );
+  },
+};
