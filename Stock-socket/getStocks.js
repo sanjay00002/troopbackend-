0ef2bcee1563ff8909f95b4c15b041dee92ae0ca
Pratik Nimje <pratiknimje@troopapp.in>
2023-07-20 14:03:40 +0530

    contest score calculation api created

--- Stock-socket/getStocks.js
+++ Stock-socket/getStocks.js
@@ -1,51 +1,58 @@
-
 const { WebSocketV2 } = require('./smartapi-javascript-main/lib');
 
-module.exports = async function(io,socket,stock_token) {
-	const web_socket = new WebSocketV2({
-		clientcode: 'P51775178',
-		jwttoken: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IlA1MTc3NTE3OCIsInJvbGVzIjowLCJ1c2VydHlwZSI6IlVTRVIiLCJpYXQiOjE2ODk1ODY3NzEsImV4cCI6MTY4OTY3MzE3MX0.Y5cQsO6NpxG4dAB-T5xiBC3Lh0MX-TXGA9sy2o1G8DAeco0uu-XrI1hO7cLvLtsAKGzU0lGP-kH394NOs_ZePQ',
-		apikey: 'lLDi3XyK',
-		feedtype: 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IlA1MTc3NTE3OCIsImlhdCI6MTY4OTU4Njc3MSwiZXhwIjoxNjg5NjczMTcxfQ.KLOkqidhO3ybOVLVGL14bs9_zl5UMOM1G0Y7Hvcr9AAvfef6JG_k-FY2OgIYYs0Wm-JEx_LgQfL1esujeGYW9Q',
-	});
+module.exports = function (io, socket, stock_token, isLive) {
+  return new Promise((resolve, reject) => {
+    const web_socket = new WebSocketV2({
+      clientcode: 'P51775178',
+      jwttoken:
+        'Bearer eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IlA1MTc3NTE3OCIsInJvbGVzIjowLCJ1c2VydHlwZSI6IlVTRVIiLCJpYXQiOjE2ODk4Mjg4MzUsImV4cCI6MTY4OTkxNTIzNX0.O227_a7CSk5dn7z5Ly5ag_O7OnfBLMGeADnMGCbk6Inip3nAnwskvlacuYN6HFFxESJvBBvmG2ayKfiwjB_7Hw',
+      apikey: 'lLDi3XyK',
+      feedtype:
+        'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VybmFtZSI6IlA1MTc3NTE3OCIsImlhdCI6MTY4OTgyODgzNSwiZXhwIjoxNjg5OTE1MjM1fQ.vFjuVrobYOiJ8v9yMrhXY1voNg8c_nIWkcjUvg97nfMrwWcjRzYrg2bjl9S_7LPHGht9lslUd_JNW9VIv4diUw',
+    });
 
-	
-	if(stock_token[0]){
+    if (stock_token[0]) {
+      try {
+        web_socket.connect().then(() => {
+          const json_req = {
+            correlationID: 'abcde12345',
+            action: 1,
+            mode: 2,
+            exchangeType: 1,
+            tokens: stock_token,
+          };
 
-		try {
-			await web_socket.connect().then(() => {
-				const json_req = {
-					correlationID: 'abcde12345',
-					action: 1,
-					mode: 2,
-					exchangeType: 1,
-					tokens: stock_token,
-				};
-		
-				web_socket.fetchData(json_req);
-		
-				web_socket.on('tick', receiveTick);
-		
-				// console.log(socket.nsp.name);
-				function receiveTick(data) {
-					// console.log("socket file");
-					console.log('receiveTick:::::', data);
+          web_socket.fetchData(json_req);
 
-					io.of("/liveContest").emit('get-live-data',data)
-					io.of("/normalContest").emit('get-live-data',data)
-				}
-			});
-			// socket.on('disconnect', () => {
-			// 	console.log('A client disconnected');
-			// 	web_socket.close();
-			// 	console.log("connection closed");
-			// });
-		} catch (error) {
-			console.log("Error while fetching data",error);
-		}
-	} else {
-		web_socket.close();
-		console.log("connection closed");
-	}
-}
+          web_socket.on('tick', receiveTick);
 
+          // console.log(socket.nsp.name);
+          const stock_data = [];
+
+          function receiveTick(data) {
+            // console.log("socket file");
+            // console.log('receiveTick:::::', data);
+            if (isLive) {
+              io.of('/liveContest').emit('get-live-data', data);
+              io.of('/normalContest').emit('get-live-data', data);
+            } else {
+              stock_data.push(data);
+              if (stock_data.length === stock_token.length) {
+                web_socket.close();
+                console.log('Connection closed');
+                resolve(stock_data);
+              }
+            }
+          }
+        });
+      } catch (error) {
+        console.log('Error while fetching data', error);
+        reject(error);
+      }
+    } else {
+      web_socket.close();
+      console.log('connection closed');
+      reject(new Error('No stock token provided'));
+    }
+  });
+};
