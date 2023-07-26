ba67368cb282f79c5223dc43589c9dac9fcbc747
DeepTTroop <deeptalekar@troopapp.in>
2023-07-26 14:54:20 +0530

    :necktie: :coffin: Update price distribution file for serving APIs

--- src/lib/payout.js
+++ src/lib/payout.js
@@ -1,17 +1,14 @@
-function calculatePayout(tp, ef, p) {
+export default function calculatePayout(tp, ef, p) {
   let totalPeople = Number(Math.floor(tp).toFixed(0)),
     TotalPeople = Number(Math.floor(tp).toFixed(0)),
     entryFee = Number(ef),
     mper = Number(p);
 
+  const priceDistribution = [];
+
   console.log('Inputs: ', totalPeople, TotalPeople, entryFee, mper);
 
   let totalAmount = totalPeople * entryFee;
-  console.log(
-    '🚀 ~ file: payout.js:10 ~ calculatePayout ~ totalAmount:',
-    totalAmount,
-  );
-
   // * Use loop to simulate number of iterations required to find final player count
   let noOfIterations;
 
@@ -23,11 +20,6 @@ function calculatePayout(tp, ef, p) {
       break;
     }
   }
-  console.log(
-    '🚀 ~ file: payout.js:17 ~ calculatePayout ~ noOfIterations:',
-    noOfIterations,
-  );
-
   let list_3 = [];
 
   for (let i = 0; i < noOfIterations + 1; i++) {
@@ -44,84 +36,34 @@ function calculatePayout(tp, ef, p) {
   for (let i = 0; i < list_3.length - 1; i++) {
     list_6[i] = list_5[i + 1] - list_5[i];
   }
-
-  console.log('🚀 ~ file: payout.js:32 ~ calculatePayout ~ list_3:', list_3);
-  console.log('🚀 ~ file: payout.js:44 ~ calculatePayout ~ list_5:', list_5);
-  console.log('🚀 ~ file: payout.js:45 ~ calculatePayout ~ list_6:', list_6);
-
   // * Calculate the inverse square of the player count after each iteration
   let list_semi = list_5.map((y) => Math.pow(y, -2));
-  console.log(
-    '🚀 ~ file: payout.js:53 ~ calculatePayout ~ list_semi:',
-    list_semi,
-  );
-
   // * Calculate weight for each rank
   let weights = [];
 
   for (let i = 0; i < list_6.length; i++) {
     weights[i] = list_6[i] * list_semi[i];
   }
-  console.log('🚀 ~ file: payout.js:59 ~ calculatePayout ~ weights:', weights);
-
   // * Calculate sum of weights
   let sum_weights = 0;
   for (let i = 0; i < weights.length; i++) {
     sum_weights += weights[i];
   }
-  console.log(
-    '🚀 ~ file: payout.js:66 ~ calculatePayout ~ sum_weights:',
-    sum_weights,
-  );
-
   // * Create a new list containing the payout for each player
   // * Payout is calculated by multiplying the weight with the additional payout and dividing the result by the sum of the weights
   // * The entry fee is added to each payout value in the list
   let weights_demi = weights;
   let minimum_distribution = mper * Number(Math.floor(totalAmount).toFixed(0));
-  console.log(
-    '🚀 ~ file: payout.js:81 ~ calculatePayout ~ weights_demi:',
-    weights_demi,
-  );
-  console.log(
-    '🚀 ~ file: payout.js:86 ~ calculatePayout ~ minimum_distribution:',
-    minimum_distribution,
-  );
-
   let additional_payout =
     (1 - mper) * Number(Math.floor(totalAmount).toFixed(0));
-  console.log(
-    '🚀 ~ file: payout.js:92 ~ calculatePayout ~ additional_payout:',
-    additional_payout,
-  );
-
   weights_demi = weights_demi.map((y) => y * additional_payout);
-  console.log(
-    '🚀 ~ file: payout.js:99 ~ calculatePayout ~ weights_demi:',
-    weights_demi,
-  );
   weights_demi = weights_demi.map((y) => y / sum_weights);
-  console.log(
-    '🚀 ~ file: payout.js:104 ~ calculatePayout ~ weights_demi:',
-    weights_demi,
-  );
-
   for (let i = 0; i < list_6.length; i++) {
     weights_demi[i] = weights_demi[i] / list_6[i];
   }
-  console.log(
-    '🚀 ~ file: payout.js:111 ~ calculatePayout ~ weights_demi:',
-    weights_demi,
-  );
-
   weights_demi = weights_demi.map((y) =>
     Number(Math.floor(y + Number(Math.floor(entryFee).toFixed(0))).toFixed(0)),
   );
-  console.log(
-    '🚀 ~ file: payout.js:120 ~ calculatePayout ~ weights_demi:',
-    weights_demi,
-  );
-
   // * Calculate payout for each rank and print it
   // * If there is a gap in the ranking, the payout is calculated by multiplying the difference between the adjacent ranking values
   // * with the weight for the higher ranking
@@ -137,16 +79,28 @@ function calculatePayout(tp, ef, p) {
           weights_demi[i]
         }`,
       );
+
+      priceDistribution.push({
+        rankStart: list_5[i - 1] + 1,
+        rankEnd: list_5[i],
+        priceAmount: weights_demi[i],
+      });
     } else {
       sum += weights_demi[i];
+      priceDistribution.push({
+        rankStart: list_5[i],
+        rankEnd: list_5[i],
+        priceAmount: weights_demi[i],
+      });
       console.log(`Rank: ${list_5[i]}\t\t Payout: ${weights_demi[i]}`);
     }
   }
-  console.log('🚀 ~ file: payout.js:129 ~ calculatePayout ~ sum:', sum);
   console.log('Payout to players: ', sum);
   console.log('Payout to organizers: ', totalAmount - sum);
-}
+  console.log(
+    'Price Distribution: ',
+    JSON.stringify(priceDistribution, null, 4),
+  );
 
-// Usage example:
-// You can call the function with the appropriate arguments like this:
-calculatePayout(100, 12, 0.7);
+  return priceDistribution;
+}
