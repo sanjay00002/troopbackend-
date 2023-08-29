147b313fbe768ae995655d5be1123177ec02390a
tanishq14developer <tanishq.happy@gmail.com>
2023-08-29 11:39:14 +0530

    Feat:updated

--- websockets/stockSocket/generate_token.mjs
+++ websockets/stockSocket/generate_token.mjs
@@ -20,7 +20,7 @@ const smart_api = new SmartAPI({
 const client_id = process.env.SMARTAPI_CLIENT_CODE;
 const client_password = process.env.SMARTAPI_CLIENT_PASSWORD;
 smart_api
-  .generateSession(client_id, client_password, "220718")
+  .generateSession(client_id, client_password, "020409")
   .then((data) => {
     // jwt = data.data.jwtToken;
     console.log("Tokens: ", data.data);
