8329ae6a0bb2782d6578178ae7b3761831748f92
Shashank <ginni003@gmail.com>
2023-11-20 20:40:13 +0530

    untested commit

--- websockets/stockSocket/generate_token.mjs
+++ websockets/stockSocket/generate_token.mjs
@@ -20,7 +20,7 @@ const smart_api = new SmartAPI({
 const client_id = process.env.SMARTAPI_CLIENT_CODE;
 const client_password = process.env.SMARTAPI_CLIENT_PASSWORD;
 smart_api
-  .generateSession(client_id, client_password, "555185")
+  .generateSession(client_id, client_password, "484631")
   .then((data) => {
     // jwt = data.data.jwtToken;
     console.log("Tokens: ", data.data);
